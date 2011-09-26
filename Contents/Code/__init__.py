de4be307025f47be1d142df1f211c8d0937be8bf
Pierre <p.dellanave@gmail.com>
2011-09-25 23:41:36 -0700

    Fixed video url parsing and accent handling

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -26,12 +26,14 @@ PLUGIN_ICON_PREFS					= "icon-default.png"
 # Strings - Here since there is a bug with the JSON loading
 ANEPASMANQUER_MENU_TITLE			= "À ne pas manquer".decode('utf-8')
 ANEPASMANQUER_MENU_SUMMARY			= "Les Bandes-Annonces des films attendus".decode('utf-8')
-CETTESEMAINE_MENU_TITLE			= "Sorties de la semaine".decode('utf-8')
+CETTESEMAINE_MENU_TITLE			    = "Sorties de la semaine".decode('utf-8')
 CETTESEMAINE_MENU_SUMMARY			= "Les Bandes-Annonces des films sortis mercredi dernier.".decode('utf-8')
 PROCHAINEMENT_MENU_TITLE			= "Prochainement".decode('utf-8')
 PROCHAINEMENT_MENU_SUMMARY			= "Les Bandes-Annonces des films devant sortirs dans les semaines à venir.".decode('utf-8')
 ALAFFICHE_MENU_TITLE				= "À l'affiche".decode('utf-8')
 ALAFFICHE_MENU_SUMMARY				= "Les Bandes-Annonces des films en salle actuellement.".decode('utf-8')
+
+VIDEODETAILS = 'http://www.allocine.fr/modules/video/xml/AcVisiondata.ashx?media=%s&ref=191004&typeref=film'
 
 # Plugin Artwork
 PLUGIN_ARTWORK						= "art-default.jpg"
@@ -87,24 +89,33 @@ def TraiteFluxRSS(urlFluxRSS, titreFluxRSS):
 
 	fluxXML = XML.ElementFromURL(urlFluxRSS,encoding = "iso-8859-1")
 	
-	for c in fluxXML.xpath(".//item"):
+	for c in fluxXML.xpath(".//item",encoding = "iso-8859-1"):
 		titleEtSubtitle = c.xpath('.//title')[0].text.rsplit(' - ',1)
-		title = titleEtSubtitle[0]
-		subtitle = titleEtSubtitle[1]
-		thumb = c.find('enclosure').get('url')
-		thumb_type = c.find('enclosure').get('type')
+		title = titleEtSubtitle[0].encode("iso-8859-1")
+		try:
+		  subtitle = titleEtSubtitle[1].encode("iso-8859-1")
+		except:
+		  subtitle = None
+		try:
+		  thumb = c.find('enclosure').get('url')
+		  thumb_type = c.find('enclosure').get('type')
+		except:
+		  thumb = c.find('thumbnail').get('url')
+		  thumb_type = "image/jpeg"
 		
-		urlFlv = "http://hd.fr.mediaplayer.allocine.fr/nmedia/" + thumb.rsplit("acmedia/medias/nmedia/")[1].rsplit(".jpg")[0] + "_hd_001.flv"
+		FilmPath  = thumb.rsplit("medias/nmedia/")[1].rsplit(".jpg")[0]
+		FilmId = FilmPath.split('/')[-1].split('_')[0]
+		
+		urlFlv = XML.ElementFromURL(VIDEODETAILS%FilmId).xpath('//AcVisionVideo')[0].get('hd_path')
 
-		urlDescription = "http://www.allocine.fr/film/fichefilm_gen_cfilm=" + c.find('link').text.rsplit("cfilm=")[1]
 		try :
-		  urlDescription = "http://www.allocine.fr/film/fichefilm_gen_cfilm=" + c.find('link').text.rsplit("cfilm=")[1]
+		  urlDescription = "http://www.allocine.fr/film/fichefilm_gen_cfilm=" + XML.StringFromElement(c.xpath("./link")[0]).rsplit("cfilm=")[1]
 		
 		  pageFilm = HTML.ElementFromURL(urlDescription,encoding = "iso-8859-1")
 		  divDescription = pageFilm.xpath('//p[contains(., "Synopsis :")]')
 		  description = divDescription[0].xpath("string()").rsplit("Synopsis :")[1].encode("iso-8859-1")
 		except:
-		  description = ''
+		  description = c.find('description').text_content().split('<')[0].encode("iso-8859-1")
 		
 		dir.Append(VideoItem(urlFlv, title=title, subtitle=subtitle, summary=description, thumb=Function(GetThumb,path=thumb,thumb_type=thumb_type))) 
 	
